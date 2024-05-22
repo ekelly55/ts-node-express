d2f1ec5d286b39d35b49fbff554b50e7a89cc1ed
ekelly55 <eamonn.michael.kellyr@email.com>
2024-05-22 14:52:33 -0400

    fixed nodemon/--respawn incompatibility. switched to just ts-node-dev

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server updated again: user db done');
+  res.send('fixed nodemon/--respawn incompatibility');
 });
 
 app.listen(port, () => {
