59dbce0f4345db5796560c8d01858ea7b9df8f41
ekelly55 <eamonn.michael.kellyr@email.com>
2024-05-22 16:56:38 -0400

    get one, get all, register, login and delete routes and functions working

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, {Express, Request, Response} from 'express';
+import express, {Express}  from 'express';
 import dotenv from 'dotenv'
 import cors from 'cors'
 import helmet from 'helmet'
@@ -19,9 +19,9 @@ app.use(helmet())
 app.use('/', userRouter)
 
 // this part is now handled by the router?
-app.get('/', (req: Request, res: Response) => {
-  res.send('server fixed');
-});
+// app.get('/', (req: Request, res: Response) => {
+//   res.send('server fixed');
+// });
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
